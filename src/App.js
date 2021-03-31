588a2d0a7bf48cac1930098968deeccea710fae3
laurapoc <laura.pociuniene@gmail.com>
2021-03-31 21:58:55 +0300

    add unstyled slider

--- src/App.js
+++ src/App.js
@@ -6,10 +6,9 @@ import { generatePalette } from "./colorHelpers";
 
 export default class App extends Component {
   render() {
-    console.log(generatePalette(seedColors[4]));
     return (
       <div>
-        <Palette {...seedColors[4]} />
+        <Palette palette={generatePalette(seedColors[4])} />
       </div>
     );
   }
