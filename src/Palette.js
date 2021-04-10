996f20127dc654fb5f4a4b312ce49c62ef19a176
laurapoc <laura.pociuniene@gmail.com>
2021-04-10 14:07:28 +0300

    add Select

--- src/Palette.js
+++ src/Palette.js
@@ -10,24 +10,29 @@ export default class Palette extends Component {
 
     this.state = {
       level: 500,
+      format: "hex",
     };
     this.changeLevel = this.changeLevel.bind(this);
+    this.changeFormat = this.changeFormat.bind(this);
   }
 
   changeLevel(level) {
     this.setState({ level });
-    // console.log(level);
+  }
+
+  changeFormat(val) {
+    this.setState({ format: val });
   }
 
   render() {
     const { colors } = this.props.palette;
-    const { level } = this.state;
+    const { level, format } = this.state;
     const colorBoxes = colors[level].map((color) => (
-      <ColorBox key={color.name} background={color.hex} name={color.name} />
+      <ColorBox key={color.name} background={color[format]} name={color.name} />
     ));
     return (
       <div className="Palette">
-        <Navbar level={level} changeLevel={this.changeLevel} />
+        <Navbar level={level} changeLevel={this.changeLevel} handleChange={this.changeFormat} />
         {/* Navbar goes here */}
         <div className="Palette-colors">{colorBoxes}</div>
         {/* footer eventually */}
