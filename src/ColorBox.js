afcecb7ef94901fcd87df7a62901709e99b1fc73
Laura P <laura.pociuniene@gmail.com>
2021-04-15 11:27:28 +0300

    Add link to ColorBox and add Route

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
+import { Link } from "react-router-dom";
 import "./ColorBox.css";
 
 export default class ColorBox extends Component {
@@ -12,18 +13,21 @@ export default class ColorBox extends Component {
   }
 
   changeCopyState = () => {
-    this.setState({copied: true}, () => {
-      setTimeout(() => this.setState({copied: false}), 1500);
+    this.setState({ copied: true }, () => {
+      setTimeout(() => this.setState({ copied: false }), 1500);
     });
-  }
+  };
 
   render() {
     const { name, background } = this.props;
-    const {copied} = this.state;
+    const { copied } = this.state;
     return (
       <CopyToClipboard text={background} onCopy={this.changeCopyState}>
         <div style={{ background }} className="ColorBox">
-          <div style={{ background }} className={`copy-overlay ${copied && "show"}`}/>
+          <div
+            style={{ background }}
+            className={`copy-overlay ${copied && "show"}`}
+          />
           <div className={`copy-msg ${copied && "show"}`}>
             <h1>copied!</h1>
             <p>{this.props.background}</p>
@@ -34,7 +38,10 @@ export default class ColorBox extends Component {
             </div>
             <button className="copy-button">Copy</button>
           </div>
-          <span className="see-more">More</span>
+          {/* method stopPropagation() prevents further events from beeing propagated: */}
+          <Link to="/" onClick={e => e.stopPropagation()}>
+            <span className="see-more">More</span>
+          </Link>
         </div>
       </CopyToClipboard>
     );
