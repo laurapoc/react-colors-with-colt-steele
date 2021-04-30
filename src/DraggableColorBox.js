07e16043679576f0020590ccd8bcce38e657ef07
Laura P <laura.pociuniene@gmail.com>
2021-04-30 14:32:58 +0300

    sharing between PC

--- src/DraggableColorBox.js
+++ src/DraggableColorBox.js
@@ -11,12 +11,29 @@ const styles = {
     cursor: "pointer",
     marginBottom: "-3.9px",
   },
+  boxContent: {
+    position: "absolute",
+    width: "100%",
+    padding: "10px",
+    left: "0px",
+    bottom: "0px",
+    color: "black",
+    letterSpacing: "1px",
+    textTransform: "uppercase",
+    fontSize: "12px",
+    display: "flex",
+    justifyContent: "space-between",
+  },
 };
 
 function DraggableColorBox(props) {
+  const { classes } = props;
   return (
-    <div className={props.classes.root} style={{ backgroundColor: props.color }}>
-      {props.name}
+    <div className={classes.root} style={{ backgroundColor: props.color }}>
+      <div className={classes.boxContent}>
+        <span>{props.name}</span>
+        <span>X</span>
+      </div>
     </div>
   );
 }
