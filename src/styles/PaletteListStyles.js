4a47e6accabfc4044ac4d458583b40afb48c3b81
laurapoc <laura.pociuniene@gmail.com>
2021-05-01 19:56:55 +0300

    Make PaletteList responsive

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,3 +1,5 @@
+import sizes from "./sizes";
+
 /* eslint import/no-anonymous-default-export: [2, {"allowObject": true}] */
 export default {
   root: {
@@ -14,6 +16,12 @@ export default {
     alignItems: "flex-start",
     flexDirection: "column",
     flexWrap: "wrap",
+    [sizes.down("xl")]: {
+      width: "80%",
+    },
+    [sizes.down("xs")]: {
+      width: "70%",
+    },
   },
   nav: {
     display: "flex",
@@ -22,14 +30,21 @@ export default {
     color: "white",
     alignItems: "center",
     "& a": {
-      color: "white"
-    }
+      color: "white",
+    },
   },
   palettes: {
     boxSizing: "border-box",
     width: "100%",
     display: "grid",
     gridTemplateColumns: "repeat(3, 30%)",
-    gridGap: "5%",
+    gridGap: "2.5rem",
+    [sizes.down("md")]: {
+      gridTemplateColumns: "repeat(2, 50%)",
+    },
+    [sizes.down("xs")]: {
+      gridTemplateColumns: "repeat(1, 100%)",
+      gridGap: "1rem",
+    },
   },
 };
