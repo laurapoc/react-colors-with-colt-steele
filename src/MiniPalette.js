dd5e77158f92192a0220996341325e262bec5cd2
laurapoc <laura.pociuniene@gmail.com>
2021-05-01 23:29:50 +0300

    reafctor with PureComponent

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -1,27 +1,33 @@
-import React, { Component } from "react";
+import React, { PureComponent } from "react";
 import { withStyles } from "@material-ui/core/styles";
 import styles from "./styles/MiniPaletteStyles";
 import DeleteIcon from "@material-ui/icons/Delete";
 
-class MiniPalette extends Component {
+class MiniPalette extends PureComponent {
   constructor(props) {
     super(props);
 
     this.state = {};
     this.deletePalette = this.deletePalette.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
   deletePalette(e) {
     e.stopPropagation();
     this.props.openDialog(this.props.id);
   }
+
+  handleClick() {
+    this.props.goToPalette(this.props.id);
+  }
   render() {
-    const { classes, paletteName, emoji, colors, handleClick } = this.props;
+    console.log("rerendering", this.props.paletteName);
+    const { classes, paletteName, emoji, colors } = this.props;
     const miniColorBoxes = colors.map((color) => (
       <div className={classes.miniColor} style={{ backgroundColor: color.color }} key={color.name}></div>
     ));
     return (
-      <div className={classes.root} onClick={handleClick}>
+      <div className={classes.root} onClick={this.handleClick}>
         <DeleteIcon className={classes.deleteIcon} onClick={this.deletePalette} />
         <div className={classes.colors}>{miniColorBoxes}</div>
         <h5 className={classes.title}>
--- src/PaletteList.js
+++ src/PaletteList.js
@@ -27,6 +27,7 @@ class PaletteList extends Component {
     this.openDialog = this.openDialog.bind(this);
     this.closeDialog = this.closeDialog.bind(this);
     this.handleDelete = this.handleDelete.bind(this);
+    this.goToPalette = this.goToPalette.bind(this);
   }
 
   openDialog(id) {
@@ -60,7 +61,7 @@ class PaletteList extends Component {
               <CSSTransition key={palette.id} classNames="fade" timeout={500}>
                 <MiniPalette
                   {...palette}
-                  handleClick={() => this.goToPalette(palette.id)}
+                  goToPalette={this.goToPalette}
                   key={palette.id}
                   // handleDelete={deletePalette}
                   openDialog={this.openDialog}
