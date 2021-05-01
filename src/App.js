1edd13878651d0a434b910335c02c7624bdccd52
laurapoc <laura.pociuniene@gmail.com>
2021-05-01 16:22:04 +0300

    save palettes to local storage

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ import NewPaletteForm from "./NewPaletteForm";
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { palettes: seedColors };
+    const savedPalettes = JSON.parse(window.localStorage.getItem("palettes"));
+    this.state = { palettes: savedPalettes || seedColors };
     this.savePalette = this.savePalette.bind(this);
     this.findPalette = this.findPalette.bind(this);
   }
@@ -23,9 +24,13 @@ export default class App extends Component {
   }
 
   savePalette(newPalette) {
-    this.setState({ palettes: [...this.state.palettes, newPalette] });
+    this.setState({ palettes: [...this.state.palettes, newPalette] }, this.syncLocalStorage);
   }
 
+  syncLocalStorage() {
+    //save palettes to localStorage
+    window.localStorage.setItem("palettes", JSON.stringify(this.state.palettes));
+  }
   render() {
     return (
       <Switch>
