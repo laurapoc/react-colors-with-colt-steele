4f19667db57ded0e7a7d33dccbe4bdd99dc07e5b
laurapoc <laura.pociuniene@gmail.com>
2021-05-01 15:41:24 +0300

    move styles out and add constants file

--- src/ColorPickerForm.js
+++ src/ColorPickerForm.js
@@ -4,28 +4,7 @@ import { withStyles } from "@material-ui/core/styles";
 import { ChromePicker } from "react-color";
 import Button from "@material-ui/core/Button";
 import { ValidatorForm, TextValidator } from "react-material-ui-form-validator";
-
-const styles = {
-  //this class is not in lector's contained
-  //couldn't find why picker doesn't takes 100% width either way
-  pickerContainer: {
-    width: "100%",
-  },
-  picker: {
-    width: "100% !important",
-    marginTop: "2rem",
-  },
-  addColor: {
-    width: "100%",
-    padding: "1rem",
-    marginTop: "1rem",
-    fontSize: "2rem",
-  },
-  colorNameInput: {
-    width: "100%",
-    height: "70px",
-  },
-};
+import styles from "./styles/ColorPickerFormsStyles";
 
 class ColorPickerForm extends Component {
   constructor(props) {
--- src/PaletteFormNav.js
+++ src/PaletteFormNav.js
@@ -1,59 +1,16 @@
 import React, { Component } from "react";
 import { withStyles } from "@material-ui/core/styles";
-
 import { Link } from "react-router-dom";
 import classNames from "classnames";
-
 import CssBaseline from "@material-ui/core/CssBaseline";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
-
 import Button from "@material-ui/core/Button";
-
 import PaletteMetaForm from "./PaletteMetaForm";
-
-const drawerWidth = 400;
-
-const styles = (theme) => ({
-  root: {
-    display: "flex",
-  },
-  appBar: {
-    transition: theme.transitions.create(["margin", "width"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    height: "64px",
-  },
-  appBarShift: {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: drawerWidth,
-    transition: theme.transitions.create(["margin", "width"], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  menuButton: {
-    marginLeft: 12,
-    marginRight: 20,
-  },
-  navBtns: {
-    marginRight: "1rem",
-    "& a": {
-      textDecoration: "none",
-    },
-  },
-  button: {
-    margin: "0 0.5rem",
-  },
-});
+import styles from "./styles/PaletteFormNavStyles";
 
 class PaletteFormNav extends Component {
   constructor(props) {
@@ -79,7 +36,7 @@ class PaletteFormNav extends Component {
   }
 
   hideForm() {
-    this.setState({formShowing: false});
+    this.setState({ formShowing: false });
   }
 
   render() {
