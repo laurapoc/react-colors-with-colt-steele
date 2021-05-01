95f49754e0d8b526a0f4c29b3a766f2ecfecaa4b
laurapoc <laura.pociuniene@gmail.com>
2021-05-01 13:50:04 +0300

    style PaletteMetaForm

--- src/PaletteFormNav.js
+++ src/PaletteFormNav.js
@@ -29,6 +29,7 @@ const styles = (theme) => ({
     }),
     flexDirection: "row",
     justifyContent: "space-between",
+    alignItems: "center",
     height: "64px",
   },
   appBarShift: {
@@ -43,7 +44,15 @@ const styles = (theme) => ({
     marginLeft: 12,
     marginRight: 20,
   },
-  navBtns: {},
+  navBtns: {
+    marginRight: "1rem",
+    "& a": {
+      textDecoration: "none",
+    },
+  },
+  button: {
+    margin: "0 0.5rem",
+  },
 });
 
 class PaletteFormNav extends Component {
@@ -52,14 +61,20 @@ class PaletteFormNav extends Component {
 
     this.state = {
       newPaletteName: "",
+      formShowing: false,
     };
     this.handleChange = this.handleChange.bind(this);
+    this.showForm = this.showForm.bind(this);
   }
 
   handleChange(evt) {
     this.setState({
       [evt.target.name]: evt.target.value,
     });
+  }
+
+  showForm() {
+    this.setState({ formShowing: true });
   }
 
   render() {
@@ -89,14 +104,17 @@ class PaletteFormNav extends Component {
             </Typography>
           </Toolbar>
           <div className={classes.navBtns}>
-            <PaletteMetaForm palettes={palettes} handleSumbit={handleSumbit} />
             <Link to="/">
-              <Button variant="contained" color="secondary">
+              <Button variant="contained" color="secondary" className={classes.button}>
                 Go Back
               </Button>
             </Link>
+            <Button variant="contained" color="primary" onClick={this.showForm} className={classes.button}>
+              Save
+            </Button>
           </div>
         </AppBar>
+        {this.state.formShowing && <PaletteMetaForm palettes={palettes} handleSumbit={handleSumbit} />}
       </div>
     );
   }
